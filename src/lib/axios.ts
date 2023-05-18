f0057467d6de5a331485bd23753da02f0a907824
kaz <kazuki.amatatsu.fd@gmail.com>
2023-05-19 01:05:29 +0900

    add api route client

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+export const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 export const openaiClient = axios.create({
   baseURL: "https://api.openai.com/v1/completions",
   headers: {
